bd0449fbe6f2c10b026788e072b16206e88b2bc2
Artyom Artemiev <cptn.foxmk@gmail.com>
2016-08-30 22:27:36 +0200

    Update brew.sh

--- osx/brew.sh
+++ osx/brew.sh
@@ -5,14 +5,35 @@ brew update
 brew upgrade --all
 
 # Install programs
+brew install autoenv
 brew install bash
-brew install zsh
+brew install git
+brew install git-flow
+brew install go
+brew install httpie
+brew install node
+brew install nvm
+brew install pass
 brew install python
 brew install python3
-brew install go
-
-brew install git
 brew install ssh-copy-id
+brew install sqlite
+brew install zsh
+
+brew cask install atom
+brew cask install docker
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install gpgtools
+brew cask install hyperswitch
+# brew cask install intellij-idea
+brew cask install iterm2
+brew cask install java
+brew cask install skype
+brew cask install sourcetree
+brew cask install telegram
+brew cask install vagrant
+brew cask install virtualbox
 
 # Remove outdated versions from the cellar.
 brew cleanup
